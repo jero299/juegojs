991c6e99853375db683ba648ba14447b1f50e64c
Juan Lorenzo Reyes Silva <lorenzoreyes62@aragon.unam.mx>
2023-02-09 00:02:32 -0600

    Terminada la física del primer objeto

--- src/juego.js
+++ src/juego.js
@@ -2,6 +2,31 @@ const canvas = document.getElementById('canvas');
 canvas.width = 1024;
 canvas.height = 576;
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+// Framerate y posicion
+let y = 0;
+let x = 0;
+function animate () {
+    window.requestAnimationFrame(animate);
+    // console.log("Hello")
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(x,y,100,100);
+    checkCollision();
+}
+
+// Colisiones -- Es - 100 porque el rectangulo es de 100x100
+function checkCollision() {
+    if (y > canvas.height - 100) {
+        y = canvas.height - 100;
+    }
+    y += 13 ;
+}
+
+
+
+animate();
 
