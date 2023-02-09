6b6c91119399bc39e9716d80fa54481afd8c8072
Juan Lorenzo Reyes Silva <lorenzoreyes62@aragon.unam.mx>
2023-02-08 23:43:49 -0600

    Se corrigió la posición del canvas, su tamaño y sus propiedades

--- src/juego.js
+++ src/juego.js
@@ -1,1 +1,7 @@
-console.log("funcionando")
+const canvas = document.getElementById('canvas');
+canvas.width = 1024;
+canvas.height = 576;
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
